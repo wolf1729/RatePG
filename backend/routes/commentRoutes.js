bd465e771b0ac487109735f5f205723fa57f0644
wolf1729 <aryanm1729@gmail.com>
2024-10-07 14:01:14 +0530

    adding token verification in rest of the routes

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,9 +1,10 @@
 const express= require('express')
 const commentRouter = express.Router()
 const commentController = require('../controllers/commentController')
+const verifyToken = require('../utils/verifyToken')
 
 //Route to add new Comment
-commentRouter.post('/addNewComment', commentController.addNewComment)
+commentRouter.post('/addNewComment', verifyToken.verifyToken, commentController.addNewComment)
 
 //Route to get all comments of specific PG
 commentRouter.post('/pgComments', commentController.showComments)
