236e954dbb9064cb72e2037cbbf5986b9ac169ea
wolf1729 <aryanm1729@gmail.com>
2025-01-23 01:07:31 +0530

    solving the reload issue

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: '/index.html',
-    },
+    outDir: 'dist',
   },
   server: {
     historyApiFallback: true,
