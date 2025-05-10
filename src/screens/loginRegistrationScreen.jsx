da98597a05a3459702483cacb94503d3926b7e3c
wolf1729 <aryanm1729@gmail.com>
2025-05-11 00:49:56 +0530

    fixed the scrolling problem on the login/registration screen

--- src/screens/loginRegistrationScreen.jsx
+++ src/screens/loginRegistrationScreen.jsx
@@ -11,13 +11,13 @@ function LoginRegistrationScreen() {
     }
 
     return (
-        <div className=' w-[100vw] h-[100vh]'>
-            <div className="flex items-center justify-center space-x-4 my-3 h-[10%] w-[100%]">
+        <div className=' w-[100vw] h-[100vh] overflow-hidden'>
+            <div className="flex items-center justify-center space-x-4 my-3 h-[8%] w-[100%]">
                 <span className="font-semibold">Login</span>
                 <Switch checked={isChecked} onChange={handleToggle} />
                 <span className="font-semibold">Registration</span>
             </div>
-            <div className="flex items-center justify-center w-[100%] h-[89%]">
+            <div className="flex items-center justify-center w-[100%] h-[90%]">
                 {isChecked ? <RegistrationComponent /> : <LoginComponent />}
             </div>
         </div>
